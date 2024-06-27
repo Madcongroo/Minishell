0ae28460f1a8011413febf599279d3b9ec614008
tmontani25 <teomontani27@gmail.com>
2024-06-27 14:10:28 +0200

    before pull proton

--- parsing/tokening_line.c
+++ parsing/tokening_line.c
@@ -10,9 +10,27 @@
 
 #include "../minishell.h"
 
-
+void	ft_strtok(t_tocken *node, char *line, char del1, char del2)
+{
+	static int index;
+	static int	len;
+	while (line[len] != del1 && line[len] != del2 && line[len] != '\0')
+		len++;
+	node->arg = malloc(sizeof(char) * len);
+	len++;
+	while (line[index] != del1 && line[index] != del2 && line[index] != '\0')
+	{
+		node->arg[index] = line[index];
+		index++;
+	}
+	index++;
+	return ;
+	// allouer memoire token
+	//assigner 
+}
 void	tokening_line(t_tocken **tockens, char *line)
 {
-	*tockens = NULL;
-	node = newnode();
+	new_node(tockens);
+
+	ft_strtok(*tockens, line, ' ', '"');
 }
